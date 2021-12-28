cf62296e00ac2f76bf291ce2b30eb610ee5a06a3
ramiro350 <ramirofilho18@gmail.com>
2021-12-27 21:32:11 -0300

    primeiro commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    SurfaceEffector2D surfaceEffector2D;
+    Rigidbody2D rb2d;
+
+    bool CanMove = true;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CanMove)
+        {
+        RotatePlayer();
+        RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        CanMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
